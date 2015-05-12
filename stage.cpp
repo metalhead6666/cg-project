63bac00e1bdd20b3c426afc9cace6a6841f6c0be
metalhead6666 <noe.godinho.92@gmail.com>
2015-05-12 17:48:09 +0100

    Camera updated. Next, create the world

--- stage.cpp
+++ stage.cpp
@@ -1,7 +1,22 @@
 #include "stage.h"
 
 Stage::Stage(){
+    this->fov = 70.0;
+    this->aspect = (GLdouble)(WSCREEN / HSCREEN);
+    this->near = 1.0;
+    this->far = 1000.0;
 
+    this->r_vision = 3.0;
+    this->a_vision = 0.5 * PI;
+    this->inc_vision = 0.04;
+
+    this->obs_begin.x = 0.0;
+    this->obs_begin.y = 1.2;
+    this->obs_begin.z = -1.0;
+
+    this->obs_end.x = this->obs_begin.x - this->r_vision * cos(this->a_vision);
+    this->obs_end.y = this->obs_begin.y;
+    this->obs_end.z = this->obs_begin.z - this->r_vision * sin(this->a_vision);
 }
 
 Stage::~Stage(){
@@ -14,14 +29,35 @@ GLvoid Stage::start_stage(){
     glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - WSCREEN) / 2, (glutGet(GLUT_SCREEN_HEIGHT) - HSCREEN) / 2);
     glutCreateWindow("Crappy name ");
 
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearColor(BLACK);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(this->fov, this->aspect, this->near, this->far);
+    glMatrixMode(GL_MODELVIEW);
+    glEnable(GL_DEPTH_TEST);
     glShadeModel(GL_SMOOTH);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
 GLvoid Stage::display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(this->obs_begin.x, this->obs_begin.y, this->obs_begin.z, this->obs_end.x, this->obs_end.y, this->obs_end.z, 0.0, 1.0, 0.0);
+    glViewport(0, 0, WSCREEN, HSCREEN);
+
+    this->draw_world();
 
     glutSwapBuffers();
+}
+
+GLvoid Stage::draw_world(){
+    glPushMatrix();
+        glColor4d(WHITE);
+        glBegin(GL_POLYGON);
+
+        glEnd();
+    glPopMatrix();
 }
 
 GLvoid Stage::key_pressed(unsigned char key){
--- stage.h
+++ stage.h
@@ -1,14 +1,31 @@
 #ifndef STAGE_H
 #define STAGE_H
 
-#define WSCREEN 1280
-#define HSCREEN 720
-
 #ifdef __APPLE__
     #include <GLUT/glut.h>
 #else
     #include <GL/glut.h>
 #endif
+#include <cmath>
+
+/* colors */
+#define BLUE			 0.0, 0.0, 1.0, 1.0
+#define RED				 1.0, 0.0, 0.0, 1.0
+#define YELLOW			 1.0, 1.0, 0.0, 1.0
+#define GREEN			 0.0, 0.8, 0.0, 1.0
+#define ORANGE			 1.0, 0.5, 0.1, 1.0
+#define BROWN			 0.3, 0.2, 0.0, 1.0
+#define WHITE			 1.0, 1.0, 1.0, 1.0
+#define BLACK			 0.0, 0.0, 0.0, 1.0
+
+#define WSCREEN 1280
+#define HSCREEN 720
+
+#define PI 3.141529
+
+struct observer{
+    GLdouble x, y, z;
+};
 
 class Stage{
 public:
@@ -17,12 +34,19 @@ public:
 
     GLvoid start_stage();
     GLvoid display();
+    GLvoid draw_world();
     GLvoid key_pressed(unsigned char key);
     GLvoid key_not_pressed(unsigned char key);
     GLvoid special_key_pressed(GLint key);
     GLvoid special_key_not_pressed(GLint key);
     GLvoid mouse_motion(GLint x, GLint y);
     GLvoid click_mouse(GLint button, GLint state);
+
+private:
+    struct observer obs_begin;
+    struct observer obs_end;
+    GLdouble a_vision, r_vision, inc_vision;
+    GLdouble fov, aspect, near, far;
 };
 
 #endif // STAGE_H
